ac0ab16b26a7d5d2e26541829ab3487c0480847f
Joerg Beekmann <joerg@purepaces.com>
2022-03-21 08:00:41 -0700

    initial commit

---
+++ src/App/Program.fs
@@ -0,0 +1,7 @@
+﻿open SourceGenedRegex
+
+printf "Provide a phonenumber: "
+let input = System.Console.ReadLine()
+if PhoneNumbers.TenDigitNumber().IsMatch(input)
+then printfn "%s is a phonenumber." input
+else printfn "%s is not a phonenumber" input
---
+++ src/Regex/PhoneNumbers.cs
@@ -0,0 +1,7 @@
+﻿namespace SourceGenedRegex;
+using System.Text.RegularExpressions;
+public partial class PhoneNumbers  // <-- Make the class a partial class
+{
+  [RegexGenerator(@"^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$", RegexOptions.IgnoreCase)] // <-- Add the RegexGenerator attribute and pass in your pattern and options
+  public static partial Regex TenDigitNumber(); //  <-- Declare the partial method, which will be implemented by the source generator
+}
